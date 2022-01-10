1b4c798ef0ea230e0e396b16c9d7af1cd0a3816d
Bohdan K <bohdan.kalenichenko@gmail.com>
2022-01-10 20:59:54 +0200

    refactor images

--- src/components/MainPage/components/DragAndDropZone/mocks/mockImages.ts
+++ src/components/MainPage/components/DragAndDropZone/mocks/mockImages.ts
@@ -1,27 +1,27 @@
 const MOCK_IMAGES = [
   {
     name: "SPANNKOPF*SPH 20",
-    url: "/src/components/MainPage/components/DragnDropZone/mocks/1.jpg"
+    url: "/assets/images/1.png"
   },
   {
     name: "SPANNKOPF*AUFLAGEKOPF",
-    url: "/src/components/MainPage/components/DragnDropZone/mocks/2.jpg"
+    url: "/assets/images/2.png"
   },
   {
     name: "RING*112*55.2*19.4*/3/DREHVERT",
-    url: "/src/components/MainPage/components/DragnDropZone/mocks/3.jpg"
+    url: "/assets/images/3.png"
   },
   {
     name: "RING*138*54.2*66",
-    url: "/src/components/MainPage/components/DragnDropZone/mocks/4.jpg"
+    url: "/assets/images/4.png"
   },
   {
     name: "RING*164*86.5*17.5",
-    url: "/src/components/MainPage/components/DragnDropZone/mocks/5.jpg"
+    url: "/assets/images/5.png"
   },
   {
     name: "RING*164*86.5*17.5",
-    url: "/public/assets/images/6.png"
+    url: "/assets/images/6.png"
   }
 ];
 
